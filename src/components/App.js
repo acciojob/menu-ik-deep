eddba79146f82d25ad72ec0dd3e67aef11ed8850
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-12-13 15:48:40 +0000

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,42 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useEffect, useState} from "react";
+import {data} from "../data";
+import MenuDetails from "../components/MenuDetails";
+import "./menuList.css"
+
+
+const App=()=>{
+ const [FilterData,setFilterData] = useState("")
+ const [selectedCategory,setSelectedCategory] = useState("");
+
+
+const selectCategory = (event) => {
+       setSelectedCategory(event.target.textContent.toLowerCase());
+}
+useEffect(()=>{
+        if(selectedCategory!="" && selectedCategory!="all"){
+            setFilterData(data.filter(d=> d.category==selectedCategory));
+        }else{
+            setFilterData(data);
+        }
+},[selectedCategory])
+console.log(setFilterData);
+    return(
+        <div className="main">
+            <div className="header">
+            <h1>Our Manu</h1>
+        <ul className="menu">
+            <li className="menu-item" onClick={selectCategory}>All</li>
+            <li className="menu-item" onClick={selectCategory}>Breakfast</li>
+            <li className="menu-item" onClick={selectCategory}>Lunch</li>
+            <li className="menu-item" onClick={selectCategory}>Shakes</li>
+        </ul>
+            </div>
+       <div className="
+       menuList">   <MenuDetails data={FilterData}/></div>
+     
+        </div>
+    )
+}
+
+export default App;
 
